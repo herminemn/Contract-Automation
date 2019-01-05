dd057573139298aced943eee46acb4c0cee09e7e
herminemn <hermine.mn@gmail.com>
2019-01-06 02:06:37 +0400

    Reads from the uploaded file and outputs the words on webpage

--- contractautomation/urls.py
+++ contractautomation/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from uploads import views
@@ -8,4 +10,9 @@ urlpatterns = [
     path('', views.files_list, name='files_list'),
     path('upload/', views.upload_file, name='upload_file'),
     path('upload/<int:pk>/', views.delete_file, name='delete_file'),
+    path('template/<int:upload_id>/', views.edit_file, name='edit_file'),
+    path('template/<int:upload_id>/', views.edit_file, name='edit_file'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- uploads/views.py
+++ uploads/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UploadFileForm
 from .models import DocFile
+from django.core.files import File
+from docx import Document
+import docx
+import re
+import io
 
 
 def files_list(request):
@@ -20,6 +25,24 @@ def upload_file(request):
         form = UploadFileForm()
     return render(request, 'uploads/upload.html', {
         'form': form
+    })
+
+
+def edit_file(request, upload_id):
+    instance = get_object_or_404(DocFile, id=upload_id)
+    document = Document(instance.agreement)
+    variables = []
+    for paragraph in document.paragraphs:
+        match = re.findall(r"\{(.*?)\}", paragraph.text)
+        variables.append(match)
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                match = re.findall(r"\{(.*?)\}", cell.text)
+                variables.append(match)
+    document.save('blue.pdf')
+    return render(request, 'uploads/file_detail.html', {
+        'variables': variables
     })
 
 
